aba6a0df159248f3900da38b4bea6ffffcf6d1f9
ksachin7 <isachin.imp@gmail.com>
2024-06-08 16:09:59 +0530

    created user service and updated auth

---
+++ expense-tracker/frontend/src/components/service/UserService.js
@@ -0,0 +1,85 @@
+const fetchUsers = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/users', {
+      method: 'GET',
+      // mode:'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${localStorage.getItem('token')}`, 
+      },
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    throw error;
+  }
+};
+
+const createUser = async (userData) => {
+  try {
+    const response = await fetch('http://localhost:8080/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${localStorage.getItem('token')}`, 
+      },
+      credentials: 'include',
+      body: JSON.stringify(userData),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error creating user:', error);
+    throw error;
+  }
+};
+
+const updateUser = async (userId, userData) => {
+  try {
+    const response = await fetch(`http://localhost:8080/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${localStorage.getItem('token')}`, 
+      },
+      credentials: 'include',
+      body: JSON.stringify(userData),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error updating user:', error);
+    throw error;
+  }
+};
+
+const deleteUser = async (userId) => {
+  try {
+    const response = await fetch(`http://localhost:8080/users/${userId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Authorization': `Bearer ${localStorage.getItem('token')}`, 
+      },
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // console.log(response)
+    // return await response;
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    throw error;
+  }
+};
+
+export default { fetchUsers, createUser, updateUser, deleteUser };
