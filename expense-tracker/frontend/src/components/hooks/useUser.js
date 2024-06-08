544c9b5e6d518650aa4b03dd8264210198d95f73
ksachin7 <isachin.imp@gmail.com>
2024-06-08 16:03:23 +0530

    created some custom hooks

---
+++ expense-tracker/frontend/src/components/hooks/useUser.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+const useUser = (userId) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const BASE_URI= 'http://localhost:8080'
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch(`${BASE_URI}/users/${userId}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch user');
+        }
+        const userData = await response.json();
+        setUser(userData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  const isAuthenticated = user?.role !== '';
+//   const isAuthenticated = user?.role === 'authenticated';
+//   console.log(user)
+
+  return { isLoading, user, isAuthenticated };
+};
+
+export {useUser};
